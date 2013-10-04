5bd6ce0a6dae15dbde5a8809557f73b83c832a08
Frost End <guilhermepsantanna@gmail.com>
2013-10-04 15:46:41 -0300

    starting project

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'devise'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem "rspec"
+  gem "rspec-rails"
+  gem "webrat"
+  gem "cucumber"
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberTest::Application.config.secret_token = 'cd13dd67106b797ed5d62faf0193cbe9cf4088c4c1278ab4ee0f039480492779322a62d175687f4df1a9f88ef99bd28190fc19b1aab43982b8971c4084f6cc53'
