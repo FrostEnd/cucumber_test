9cfe241910b39c0aff1f377e4b4e4c178b37c098
Frost End <guilhermepsantanna@gmail.com>
2013-10-09 11:37:45 -0300

    finished features for blog posts

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,3 +1,13 @@
 Then /^I wait "(.+)" seconds$/ do |seconds|
   sleep seconds.to_i
 end
+
+When /^I follow "(.+)" and accept alert$/ do |link|
+  click_link(link)
+  page.driver.browser.switch_to.alert.accept
+end
+
+When /^I follow "(.+)" and reject alert$/ do |link|
+  click_link(link)
+  page.driver.browser.switch_to.alert.dismiss
+end
